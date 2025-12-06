e9cbbd7bcc437ddb027bb4c2d585ef5b1d28c268
Miyorch19 <KanyTajin22@hotmail.com>
2025-12-05 19:46:28 -0600

    Switch to Railway Volume Storage: Remove Firebase customizations and add storage:link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -43,49 +43,5 @@ class AppServiceProvider extends ServiceProvider
             return ($sum % 10) == 0;
         });
 
-        // Firebase Storage Driver
-        \Illuminate\Support\Facades\Storage::extend('firebase', function ($app, $config) {
-            $storageClient = new \Google\Cloud\Storage\StorageClient([
-                'projectId' => $config['project_id'],
-                'keyFile' => [
-                    'type' => 'service_account',
-                    'project_id' => $config['project_id'],
-                    'private_key_id' => 'undefined', // No necesario para autenticación directa
-                    'private_key' => str_replace('\\n', "\n", $config['private_key']), // Fix newlines
-                    'client_email' => $config['client_email'],
-                    'client_id' => 'undefined',
-                    'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
-                    'token_uri' => 'https://oauth2.googleapis.com/token',
-                    'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
-                    'client_x509_cert_url' => 'https://www.googleapis.com/robot/v1/metadata/x509/' . urlencode($config['client_email']),
-                ],
-            ]);
-
-            $bucket = $storageClient->bucket($config['bucket']);
-            $adapter = new \League\Flysystem\GoogleCloudStorage\GoogleCloudStorageAdapter($bucket);
-            $filesystem = new \League\Flysystem\Filesystem($adapter);
-
-            return new class($filesystem, $adapter, $config, $bucket) extends \Illuminate\Filesystem\FilesystemAdapter {
-                protected $bucket;
-
-                public function __construct($driver, $adapter, $config, $bucket)
-                {
-                    parent::__construct($driver, $adapter, $config);
-                    $this->bucket = $bucket;
-                }
-
-                public function url($path)
-                {
-                    // Generar URL firmada válida por 2 horas para evitar problemas de permisos públicos
-                    // Nota: No verificamos existe() para rendimiento.
-                    try {
-                        return $this->bucket->object($path)->signedUrl(new \DateTime('+2 hours'));
-                    } catch (\Exception $e) {
-                        // Fallback en caso de error de firma
-                        return 'https://storage.googleapis.com/' . $this->config['bucket'] . '/' . $path;
-                    }
-                }
-            };
-        });
     }
 }
